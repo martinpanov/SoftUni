2bd6d8820f7e4782de683a43c108b25a2d245b4a
martinpanov <martin3107@abv.bg>
2022-05-30 16:58:51 +0300

    deleting - missing problem description

--- Programming Basics/PB-MoreExercises/triangleArea.js
+++
@@ -1,8 +0,0 @@
-function triangleArea(input) {
-    let side = Number(input[0])
-    let height = Number(input[1])
-    let face = side * height / 2
-
-    console.log(face.toFixed(2))
-}
-triangleArea(["15", "35"])
